bb09e6f346fdf802c4bf5300c25a0e9a6963b90b
Marcjmdegroot <marjcmdegroot@gmail.com>
2019-02-14 13:00:06 +0100

    Initial commit

---
+++ src/main/java/com/capgemini/set/schaaktoernooi/Schaaktoernooi/SchaaktoernooiApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.set.schaaktoernooi.Schaaktoernooi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchaaktoernooiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchaaktoernooiApplication.class, args);
+	}
+
+}
+
